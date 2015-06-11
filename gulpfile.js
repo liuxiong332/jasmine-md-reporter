14274528c2d56024d6b84f3a9ccc78421d42d347
liuxiong332 <liuxiong332@163.com>
2015-06-11 23:06:10 +0800

    replace test task with exercise task

--- gulpfile.js
+++ gulpfile.js
@@ -5,22 +5,47 @@ var jasmine = require('gulp-jasmine');
 var TerminalReporter = require('./lib/terminal-reporter').TerminalReporter;
 
 require('coffee-script/register');
-var ConsoleReporter = require('./lib/reporter');
-var fs = require('fs');
-
-if(fs.existsSync('./wo.md')) {
-  fs.unlinkSync('./wo.md')
-}
-function print(str) {
-  fs.appendFileSync('./wo.md', str);
-}
 
 gulp.task('test', function () {
   return gulp.src('./spec/*.js')
     .pipe(jasmine({
       includeStackTrace: false,
-      // reporter: new TerminalReporter({verbosity: 2, showStack: true}),
-      reporter: new ConsoleReporter({print: print, ignoreStackPatterns: 'node_modules/**'}),
+    }));
+});
+
+
+function OutFileInfo() {}
+
+OutFileInfo.prototype.removeOutFile = function() {
+  var fs = require('fs');
+  var path = require('path');
+
+  var outPath = path.resolve(__dirname, 'out');
+  if(!fs.existsSync(outPath)) {
+    fs.mkdirSync(outPath);
+  }
+
+  this.outFilePath = outFile = path.resolve(outPath, 'log.md');
+  if(fs.existsSync(outFile)) {
+    fs.unlinkSync(outFile);
+  }
+};
+
+OutFileInfo.prototype.print = function(str) {
+  fs.appendFileSync(this.outFilePath, str);
+};
+
+gulp.task('exercies', function() {
+  var ConsoleReporter = require('./lib/reporter');
+  var info = new OutFileInfo
+  info.removeOutFile()
+  return gulp.src('./spec/fixture/*.js')
+    .pipe(jasmine({
+      includeStackTrace: false,
+      reporter: new ConsoleReporter({
+        print: info.print.bind(info),
+        ignoreStackPatterns: 'node_modules/**'
+      }),
     }));
 });
 
